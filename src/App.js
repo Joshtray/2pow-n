b79c1cbc2c25f0d750f2f15ad0847c5409ae2fca
Joshtray <jesseyuchenwichi@gmail.com>
2022-05-31 08:02:33 -0700

    Added movement functionality to tiles (up, down, left, right movement)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import styled from "styled-components"
 import Game from "./components/Game";
 
 function App() {
-  useEffect(() => {
-    {!localStorage.getItem("data") && localStorage.setItem("data", JSON.stringify({1: {1: 2, 2: 4}, 2: {1: 16, 3: 16}}))}
-  }, [])
   
   return (
     <AppContainer className="App">
--- src/components/Board.js
+++ src/components/Board.js
@@ -3,68 +3,137 @@ import styled from 'styled-components'
 import Tile from './Tile'
 
 const Board = ({ restart, setRestart }) => {
-    const [tiles, setTiles] = useState(JSON.parse(localStorage.getItem("data")))
+    const rowLength = 4
+    const colLength = 4
+    const [tiles, setTiles] = useState({1: {1: 2, 2: 4, 3: 16, 4: 16}, 2: {1: 16, 3: 16}, 3: {2: 8}})
+    // const [newTiles, setnewTiles] = useState({})
+    // var newTiles = {}
+    // useEffect(() => {
+    //     console.log(JSON.parse(localStorage.getItem("data")))
+    //     if (!JSON.parse(localStorage.getItem("data"))) {
+    //         setTiles(JSON.parse(localStorage.getItem("data")))
+    //     }
+    // }, [])
+    // useEffect(() => {
+    //     localStorage.setItem("data", JSON.stringify(tiles))
+    //     console.log(localStorage.getItem("data"))
+    // }, [tiles])
+    
     useEffect(() => {
         if (restart) {
-            localStorage.setItem("data", JSON.stringify({1: {1: 2, 2: 4}, 2: {1: 16, 3: 16}}))
-            console.log(JSON.parse(localStorage.getItem("data")))
-            setTiles(JSON.parse(localStorage.getItem("data")))   
+            setTiles({1: {1: 2, 2: 2, 3: 16}, 2: {1: 16, 3: 16}, 3: {2: 4}})   
             setRestart(false)
         }
+        console.log(tiles)
+        console.log(restart)
     }, [restart])
     
     const handleKeyDown = (event) => {
-        var newTiles = tiles
-        event.preventDefault()
-        switch (event.key) {
-            case 'ArrowLeft':
-                Object.keys(newTiles).map((row) => {
-                    Object.keys(newTiles[row]).map((col) => {
-                        var newCol = parseInt(col)
-                        while (!Object.keys(newTiles[row]).includes(`${newCol - 1}`) && newCol !== 1) {
-                            newCol = newCol - 1
+        console.log(newTiles)
+        console.log(tiles)
+        var newTiles = JSON.parse(JSON.stringify(tiles))
+        console.log(JSON.parse(JSON.stringify(tiles)))
+        console.log(newTiles)
+        if (event.key == 'ArrowLeft') {
+            console.log(newTiles)
+            console.log(tiles)
+            Object.keys(newTiles).map((row) => {
+                console.log(newTiles[row])
+                Object.keys(newTiles[row]).map((col) => {
+
+                    var newCol = parseInt(col)
+                    console.log(row, col)
+                    while (!Object.keys(newTiles[row]).includes(`${newCol - 1}`) && newCol !== 1) {
+                        newCol = newCol - 1
+                    }
+                    var multip = 1
+                    if (newCol !== 1) {
+                        if (newTiles[row][newCol - 1] == newTiles[row][col]) {
+                            multip = 2
                         }
-                        var multip = 1
-                        if (newCol !== 1) {
-                            if (newTiles[row][newCol - 1] == newTiles[row][col]) {
-                                multip = 2
-                            }
-                        }
-                        var {[col]: _, ...newRow} = newTiles[row]
-                        newTiles[row] = {...newRow, [multip > 1 ? newCol - 1  : newCol ]: (newTiles[row][col] * multip)}
-                    })
+                    }
+                    console.log(newTiles[row])
+
+                    var {[col]: _, ...newRow} = newTiles[row]
+                    console.log(newRow)
+                    newTiles[row] = {...newRow, [multip > 1 ? newCol - 1  : newCol ]: (newTiles[row][col] * multip)}
+                    console.log(newTiles)
                 })
-                break;
-            case 'ArrowUp':
-                break;
-            case 'ArrowDown':
-                break;
-            case 'ArrowRight':
-                Object.keys(newTiles).map((row) => {
-                    Object.keys(newTiles[row]).map((col) => {
-                        var newCol = parseInt(col)
-                        while (!Object.keys(newTiles[row]).includes(`${newCol - 1}`) && newCol !== 1) {
-                            newCol = newCol - 1
-                        }
-                        var multip = 1
-                        if (newCol !== 1) {
-                            if (newTiles[row][newCol - 1] == newTiles[row][col]) {
-                                multip = 2
-                            }
-                        }
-                        var {[col]: _, ...newRow} = newTiles[row]
-                        newTiles[row] = {...newRow, [multip > 1 ? newCol - 1  : newCol ]: (newTiles[row][col] * multip)}
-                    })
-                })
-                break;
-            default:
-                console.log(event.key)
+            }) 
         }
-        localStorage.setItem("data", JSON.stringify(newTiles))
-        return setTiles(JSON.parse(localStorage.getItem("data")))
+    if (event.key == 'ArrowRight') {
+        Object.keys(newTiles).map((row) => {
+            Object.keys(newTiles[row]).reverse().map((col) => {
+                var newCol = parseInt(col)
+                while (!Object.keys(newTiles[row]).includes(`${newCol + 1}`) && newCol !== colLength) {
+                    newCol = newCol + 1
+                }
+                var multip = 1
+                if (newCol !== colLength) {
+                    if (newTiles[row][newCol + 1] == newTiles[row][col]) {
+                        multip = 2
+                    }
+                }
+                var {[col]: _, ...newRow} = newTiles[row]
+                newTiles[row] = {...newRow, [multip > 1 ? newCol + 1  : newCol ]: (newTiles[row][col] * multip)}
+            })
+        })
+    }
+    if (event.key == 'ArrowUp') {
+        Object.keys(newTiles).map((row) => {
+            Object.keys(newTiles[row]).map((col) => {
+                var newRow = parseInt(row)
+                while (!newTiles[newRow - 1]?.[col] && newRow !== 1) {
+                    newRow = newRow - 1
+                }
+                var multip = 1
+                if (newRow !== 1) {
+                    if (newTiles[newRow - 1][col] == newTiles[row][col]) {
+                        multip = 2
+                    }
+                }
+                var {[col]: _, ...newCol} = newTiles[row]
+                newTiles[row] = {...newCol}
+                if (!newTiles[multip > 1 ? newRow - 1 : newRow]) {
+                    newTiles[multip > 1 ? newRow - 1 : newRow] = {}
+                }
+                newTiles[multip > 1 ? newRow - 1 : newRow][col] = tiles[row][col] * multip
+            })
+        })
+    }
+    if (event.key == 'ArrowDown') {
+        Object.keys(newTiles).reverse().map((row) => {
+            Object.keys(newTiles[row]).map((col) => {
+                var newRow = parseInt(row)
+                while (!newTiles[newRow + 1]?.[col] && newRow !== rowLength) {
+                    newRow = newRow + 1
+                }
+                var multip = 1
+                if (newRow !== rowLength) {
+                    if (newTiles[newRow + 1][col] == newTiles[row][col]) {
+                        multip = 2
+                    }
+                }
+                var {[col]: _, ...newCol} = newTiles[row]
+                newTiles[row] = {...newCol}
+                if (!newTiles[multip > 1 ? newRow + 1 : newRow]) {
+                    newTiles[multip > 1 ? newRow + 1 : newRow] = {}
+                }
+                newTiles[multip > 1 ? newRow + 1 : newRow][col] = tiles[row][col] * multip
+            })
+        })
+    }
+        console.log(newTiles)
+        setTiles(newTiles)
+        return 0
     }
     useEffect(() => {
+      console.log(tiles)
+    }, [tiles])
+    
+    useEffect(() => {
         window.addEventListener('keydown', handleKeyDown)
+        return () => {window.removeEventListener('keydown', handleKeyDown)}
     }, [])
     return (
         <GameBoardContainer>
